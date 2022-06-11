a8d3fbc2edb5c91051717140ba748e5f4843bb5e
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-06-11 13:25:22 +0000

    i cant wait to use this in prod

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,52 @@
 #![forbid(unsafe_code)]
 
-use std::{io::{self, Write, Seek}, fs};
+use std::ptr::addr_of;
+
+#[cfg(unix)]
+use std::{fs, os::unix::prelude::FileExt};
+
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+use process_memory::{PutAddress, TryIntoProcessHandle};
 
 pub fn totally_safe_transmute<T, U>(v: T) -> U {
     #[repr(C)]
     enum E<T, U> {
         T(T),
-        #[allow(dead_code)] U(U),
+        #[allow(dead_code)]
+        U(U),
     }
     let v = E::T(v);
 
-    let mut f = fs::OpenOptions::new()
-        .write(true)
-        .open("/proc/self/mem").expect("welp");
-
-    f.seek(io::SeekFrom::Start(&v as *const _ as u64)).expect("oof");
-    f.write(&[1]).expect("darn");
+    //overwrite the enum discriminator in memory using conventionally considered safe functions
+    cfg_if::cfg_if! {
+        if #[cfg(target_os = "redox")] {
+            //https://gitlab.redox-os.org/redox-os/rfcs/-/blob/master/text/0004-ptrace.md
+            let own_pid: u32 = std::process::id();
+            fs::OpenOptions::new()
+                .write(true)
+                .open(format!("proc:{own_pid}/mem")).unwrap()
+                .write_all_at(&[1], addr_of!(v) as u64).unwrap();
+        } else if #[cfg(any(target_os = "windows", target_os = "macos"))] {
+            //unloved fallback
+            //unneeded "protect" call
+            let handle: process_memory::ProcessHandle = (std::process::id() as process_memory::Pid).try_into_process_handle().unwrap();
+            handle.put_address(addr_of!(v) as usize, &[1]).unwrap();
+        } else if #[cfg(unix)] {
+            //https://man7.org/linux/man-pages/man5/proc.5.html
+            fs::OpenOptions::new()
+                .write(true)
+                .open("/proc/self/mem").unwrap()
+                .write_all_at(&[1], addr_of!(v) as u64).unwrap();
+        } else {
+            unsupported
+        }
+    }
 
     if let E::U(v) = v {
         return v;
     }
 
-    panic!("rip");
+    unreachable!("rip");
 }
 
 #[test]
