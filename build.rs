32a8d93f3b81df1d6a3fb0cffbc3e97627e74490
Benjamin Herr <ben@0x539.de>
2022-10-06 22:50:39 -0700

    slight aesthetic improvements

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use std::process;
 
 fn main() {
+    println!("cargo:rerun-if-changed=helper/src/main.rs");
     if !process::Command::new("cargo")
         .args(&["build", "--release"])
         .current_dir("helper")
--- helper/src/main.rs
+++ helper/src/main.rs
@@ -1,13 +1,59 @@
-use std::{env, ptr};
-use winapi::um::{processthreadsapi::OpenProcess, memoryapi::WriteProcessMemory};
+use std::env;
+use std::io;
+use std::process;
+use std::ptr::{addr_of, addr_of_mut};
+use std::error::Error;
 
-fn main() { unsafe {
-    let mut args = env::args().skip(1);
-    let parent = args.next().unwrap().parse().unwrap();
-    let h = OpenProcess(40, 0, parent);
-    if h.is_null() { panic!("no parent"); };
-    let p: usize = args.next().unwrap().parse().unwrap();
-    let v = 1u8;
-    let r = WriteProcessMemory(h, p as _, &v as *const _ as _, 1, ptr::null_mut());
-    if r == 0 { panic!("no"); }
-} }
+use winapi::shared::minwindef::{LPVOID, LPCVOID, FALSE};
+use winapi::um::memoryapi::WriteProcessMemory;
+use winapi::um::processthreadsapi::{OpenProcess, GetCurrentProcessId};
+use winapi::um::winnt::{PROCESS_VM_OPERATION, PROCESS_VM_WRITE};
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{:?}", e);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error+Send+Sync>> {
+    // SAFETY: It's not memory-unsafe if it's someone else's process.
+    unsafe {
+        let mut args = env::args().skip(1);
+
+        let parent_pid = args.next()
+            .ok_or_else(|| "missing parent pid as first arg")?
+            .parse()
+            .map_err(|_| "couldn't parse first arg as parent pid")?;
+
+        if parent_pid == GetCurrentProcessId() {
+            return Err("not messing with our own memory".into());
+        }
+
+        let permissions = PROCESS_VM_OPERATION | PROCESS_VM_WRITE;
+        let inherit_handle = false as i32;
+        let parent_handle = OpenProcess(permissions, inherit_handle, parent_pid);
+        if parent_handle.is_null() {
+            return Err(Box::new(io::Error::last_os_error()));
+        }
+
+        let addr: usize = args.next()
+            .ok_or_else(|| "missing enum tag address as second arg")?
+            .parse()
+            .map_err(|_| "couldn't parse second arg as enum tag address")?;
+        let addr = addr as LPVOID;
+        let value = 1u8;
+        let buffer = addr_of!(value) as LPCVOID;
+        let mut count: usize = 0;
+        let result = WriteProcessMemory(parent_handle, addr, buffer, 1, addr_of_mut!(count));
+        if result == FALSE {
+            return Err(Box::new(io::Error::last_os_error()));
+        }
+
+        if count == 0 {
+            return Err("didn't manage to write that one byte".into());
+        }
+
+        Ok(())
+    }
+}
