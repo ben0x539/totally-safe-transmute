18d6e17e2a7a214c8e4b991e6f90f48dd0d432e5
Benjamin Herr <ben@0x539.de>
2022-10-08 18:54:52 -0700

    this seems a lot easier

--- src/lib.rs
+++ src/lib.rs
@@ -23,19 +23,47 @@ pub fn totally_safe_transmute<T, U>(v: T) -> U {
     #[cfg(target_os = "windows")]
     {
         use std::{env, process, fs};
-        let b = include_bytes!(r"..\helper\target\release\helper.exe");
-        let p = env::temp_dir().join("random-collision-free-name-I1G3qPvXTU4RvRML.exe");
-        fs::write(&p, b).unwrap();
+        let p = env::temp_dir().join("random-collision-free-name-I1G3qPvXTU4RvRML.ps1");
+        fs::write(&p, r#"
+            Param (
+                [int]$parentPid,
+                [UInt64]$tagAddress
+            )
+
+            Add-Type -TypeDefinition @"
+                using System;
+                using System.Runtime.InteropServices;
+
+                public static class Helper {
+                    const int PROCESS_VM_WRITE = 0x0020;
+                    const int PROCESS_VM_OPERATION = 0x0008;
+
+                    [DllImport("kernel32.dll")]
+                    public static extern IntPtr OpenProcess(int dwDesiredAccess,
+                        bool bInheritHandle, int dwProcessId);
+
+                    [DllImport("kernel32.dll", SetLastError = true)]
+                    public static extern bool WriteProcessMemory(IntPtr hProcess, ulong lpBaseAddress,
+                        byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesWritten);
+                    
+                    public static void Run(int parentPid, ulong tagAddress) {
+                        IntPtr handle = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, parentPid);
+                        WriteProcessMemory(handle, tagAddress, new byte[]{1}, 1, (IntPtr) 0);
+                    }
+                }
+            "@
+
+            [Helper]::Run($parentPid, $tagAddress) 
+        "#.replace("\n            ", "\n")).unwrap();
         let i = process::id();
-        if !process::Command::new(&p)
-            .args(&[i.to_string(), (&v as *const _ as u64).to_string()])
+        if !process::Command::new("powershell")
+            .args(&[&p.to_str().unwrap(), &i.to_string()[..], &(&v as *const _ as u64).to_string()])
             .spawn()
             .unwrap()
             .wait()
             .unwrap()
             .success() { panic!("install linux lol"); }
         fs::remove_file(&p).unwrap();
-        println!("got here");
     }
 
     if let E::U(v) = v {
